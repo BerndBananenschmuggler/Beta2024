2a11b93dae7cb80e6bf54ec6b75ce0ac3d61dd34
Timo Petzold <tpetzol2@hs-mittweida.de>
2023-04-03 20:00:38 +0200

    added basic ui

---
+++ Assets/Scenes/Tech/Timo/BuildUI.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildUI : MonoBehaviour {
+  [SerializeField] private ScriptableObject _scriptableObject;
+  [SerializeField] private Image[] _barImages;
+
+  private void Awake() {
+    //Werte holen für die einzelnen breiten der Zellen
+  }
+}
