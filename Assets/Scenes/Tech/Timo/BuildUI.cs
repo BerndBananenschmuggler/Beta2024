a082531754c9e14a24e5d069922e4375cb718715
Timo Petzold <tpetzol2@hs-mittweida.de>
2023-04-03 22:14:27 +0200

    dynamic bar, cringe code

--- Assets/Scenes/Tech/Timo/BuildUI.cs
+++ Assets/Scenes/Tech/Timo/BuildUI.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class BuildUI : MonoBehaviour {
-  [SerializeField] private ScriptableObject _scriptableObject;
+  [SerializeField] private ComboStatsSO _comboStats;
   [SerializeField] private Image[] _barImages;
 
-  private void Awake() {
-    //Werte holen für die einzelnen breiten der Zellen
+  private void Start() {
+    Debug.Log("Building UI");
+    Vector2 widthGreen = _barImages[2].rectTransform.sizeDelta = new Vector2(_comboStats.phaseThree * 500, 100);
+
+    Vector2 widthYellow =_barImages[1].rectTransform.sizeDelta = new Vector2(_comboStats.phaseTwo * 500, 100);
+    Vector3 rectTransformPositionY1 = _barImages[1].rectTransform.localPosition;
+    rectTransformPositionY1.x = -(widthGreen.x / 2 + widthYellow.x / 2);
+    _barImages[1].rectTransform.localPosition = rectTransformPositionY1;
+      
+    _barImages[3].rectTransform.sizeDelta = new Vector2(_comboStats.phaseFour * 500, 100);
+    Vector3 rectTransformPositionY2 = _barImages[3].rectTransform.localPosition;
+    rectTransformPositionY2.x = widthGreen.x / 2 + widthYellow.x / 2;
+    _barImages[3].rectTransform.localPosition = rectTransformPositionY2;
+    
+    Vector2 widthRed = _barImages[0].rectTransform.sizeDelta = new Vector2(_comboStats.phaseOne * 500, 100);
+    Vector3 rectTransformPositionR1 = _barImages[0].rectTransform.localPosition;
+    rectTransformPositionR1.x = -(widthGreen.x / 2+ widthYellow.x + widthRed.x / 2);
+    _barImages[0].rectTransform.localPosition = rectTransformPositionR1;
+
+    _barImages[4].rectTransform.sizeDelta = new Vector2(_comboStats.phaseOne * 500, 100);
+    Vector3 rectTransformPositionR2 = _barImages[4].rectTransform.localPosition;
+    rectTransformPositionR2.x = widthGreen.x / 2+ widthYellow.x + widthRed.x / 2;
+    _barImages[4].rectTransform.localPosition = rectTransformPositionR2;
   }
 }
