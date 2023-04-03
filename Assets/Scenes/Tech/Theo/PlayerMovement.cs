3a226661ddb87b2ffeeab68254dcbc80ca50abd0
Theo Kaminsky <tkaminsk@hs-mittweida.de>
2023-04-03 21:35:21 +0200

    movement done
    wasd movement works relative to camera now

--- Assets/Scenes/Tech/Theo/PlayerMovement.cs
+++ Assets/Scenes/Tech/Theo/PlayerMovement.cs
@@ -6,24 +6,41 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 
+// Theo Kaminsky, Theo#6969
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private PlayerStatsScriptable _playerStatsScriptable;
+    
     private Rigidbody _rigidbody;
     private Vector3 _velocity;
+    private Camera _camera;
+    private Vector3 _forward;
+    private Vector3 _right;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+        _forward = _camera.transform.forward;
+        _right = _camera.transform.right;
+
+        _forward.y = 0f;
+        _right.y = 0f;
+
+        _forward.Normalize();
+        _right.Normalize();
     }
 
     private void Update()
     {
-        _rigidbody.velocity = _velocity;
+        _rigidbody.velocity = _velocity * _playerStatsScriptable.movementSpeed;
     }
 
     public void HandeMovementInput(InputAction.CallbackContext context)
     {
-        Vector2 inputVector = context.ReadValue<Vector2>();
-        _velocity = new Vector3(inputVector.x, _rigidbody.velocity.y, inputVector.y);
+        // moving direction is calculated relative to the camera
+        Vector3 inputVector = context.ReadValue<Vector2>().x * _right + 
+                              context.ReadValue<Vector2>().y * _forward;
+        _velocity = inputVector;
     }
 }
--- Assets/Scenes/Tech/Theo/PlayerStatsScriptable.cs
+++ Assets/Scenes/Tech/Theo/PlayerStatsScriptable.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "PlayerStatsScriptable",
+    menuName = "ScriptableObjects/PlayerStatsScriptable")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+// Theo Kaminsky, Theo#6969
+public class PlayerStatsScriptable : ScriptableObject
+{
+    public float movementSpeed;
+    public float rotationSpeed;
 }
